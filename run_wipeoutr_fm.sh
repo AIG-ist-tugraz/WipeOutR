5eb514a7217c944f3f3f618442c1c17028bdfdbc
Lê Viết Mẫn <manleviet@gmail.com>
2022-06-09 15:55:46 +0200

    Add run.sh for wipeoutr_fm

---
+++ run_wipeoutr_fm.sh
@@ -0,0 +1,29 @@
+#
+# WipeOutR: Automated Redundancy Detection for Feature Models
+#
+# Copyright (c) 2022 AIG team, Institute for Software Technology,
+# Graz University of Technology, Austria
+#
+# Contact: http://ase.ist.tugraz.at/ASE/
+#
+
+echo "WipeOutR_FM Evalutions"
+echo ""
+
+echo "--------------------"
+echo "|CF| = 13,972"
+echo ""
+java -jar target/wipeoutr_fm-jar-with-dependencies.jar -cfg ./conf/wipeoutr_fm/wipeoutr_fm_13972.cfg
+
+echo "--------------------"
+echo "|CF| = 18,342"
+echo ""
+java -jar target/wipeoutr_fm-jar-with-dependencies.jar -cfg ./conf/wipeoutr_fm/wipeoutr_fm_18342.cfg
+
+echo "--------------------"
+echo "|CF| = 30,572"
+echo ""
+java -jar target/wipeoutr_fm-jar-with-dependencies.jar -cfg ./conf/wipeoutr_fm/wipeoutr_fm_30572.cfg
+
+echo ""
+echo "DONE"
